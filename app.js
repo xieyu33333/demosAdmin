fa80b21db58b08fa1355d3794d53ffe013625e12
xieyu03 <xieyu03@baidu.com>
2015-06-21 01:55:50 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+var koa = require('koa');
+var fs = require('fs');
+var path = require('path');
+var app = koa();
+var config = require('./config');
+var bodyHandle = require('./bodyHandle');
+
+app.use(function *(){
+    var self = this;
+    var index = './index.html';
+    if (self.request.path === '/fileList') {
+      fileList = [];
+      function walk(path){  
+          var dirList = fs.readdirSync(path);
+          dirList.forEach(function(item){
+              if(fs.statSync(path + '/' + item).isDirectory()){
+                  walk(path + '/' + item, path);
+              }
+              fileList.push({ id: path + '/' + item, path: path + '/' + item, fileName: item, pid: path });
+          });
+      }
+
+      walk(config.root);
+      self.body = fileList;
+    }
+
+    var file = '.' + decodeURI(self.request.path);
+    try {
+      if(!fs.statSync(file).isDirectory()){
+        var data = fs.readFileSync(file, {encoding: 'utf-8'});
+        var query = self.request.query;
+        if (query.source_code && file.match(/.js$/)) {
+          data = bodyHandle.addHighlight(data, 'js');
+          data = bodyHandle.insertPrism(data);
+          self.body = data;
+        }
+        else if (query.source_code && file.match(/.html$/)) {
+          data = bodyHandle.addHighlight(data, 'html');
+          data = bodyHandle.insertPrism(data);
+          self.body = data;
+        }
+        else if (query.result && file.match(/.js$/)) {
+          try {
+            self.body = eval(data) || '';
+          }
+          catch(e) {
+            self.body = e;
+          }
+        }
+        else {
+          self.body = data;
+        }
+      }
+      else {
+        self.body = '';
+      }
+    }
+    catch(e) {}
+});
+
+
+app.listen(config.port);
---
+++ bodyHandle.js
@@ -0,0 +1,17 @@
+module.exports = {
+    insertPrism: function(body) {
+        var css = '<link href="/libs/prism.css" rel="stylesheet">';
+        var js = '<script src="/libs/prism.js"></script>';
+        return css + body + js;
+    },
+
+    addHighlight: function(body, type) {
+        var type = type || 'js';
+        if (type === 'js') {
+            return '<pre><code class="language-javascript">' + body + '</code></pre>';
+        }
+        else if (type === 'html') {
+            return '<pre><code class="language-markup"><script type="prism-html-markup">' + body + '</script></code></pre>';
+        }
+    }
+}
